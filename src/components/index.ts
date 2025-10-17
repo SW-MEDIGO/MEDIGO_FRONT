d33e530f6470b1028c3b4f0f352320f166e2a206
dmd-sik <apkool12@naver.com>
2025-10-17 19:57:45 +0900

    [Feat] - Add BottomNavigation component with icons

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { Button } from "./Button";
 export { Text } from "./Text";
 export { Container } from "./Container";
 export { Header } from "./Header";
+export { BottomNavigation } from "./BottomNavigation";
